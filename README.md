7dafb861ea155aa741406c4c368fba94d3670020
EvolutionScript <info@evolutionscript.com>
2022-07-05 19:27:33 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,68 @@
+# Metamask IPN with FIAT or Crypto
+
+[MetamaskIPN](https://metamaskipn.com) allows you to receive instant payment notifications (IPN) in your site every time
+you receive a payment from Metamask wallets.
+
+This library allows you convert FIAT or other cryptocurrencies to the [supported tokens](https://metamaskipn.com/docs) of MetamaskIPN and generate
+the payment button or shopping cart link.
+
+
+**Installation:**
+
+```bash
+composer require evolutionscript/metamaskipn
+```
+
+
+**Usage:**
+
+```php
+use EvolutionScript\MetamaskIPN as MetamaskIPN;
+//Initialize MetamaskIPN Class
+$metamaskIPN = new MetamaskIPN\MetamaskIPN();
+
+//Specify cache directory to save data from FIAT rates and make a request once per day. It is optional but prevents exceeding the API usage limit. 
+$metamaskIPN->cacheDirectory(__DIR__.'/cache');
+
+//Connect with Currency Layer and as optional connect with OpenExchangeRates. The optional provider is useful if the primary provider fails.
+$metamaskIPN->currencyProviders(
+	new \EvolutionScript\CurrencyAPI\Providers\CurrencyLayer('CURRENCY_LAYER_API'),
+	new \EvolutionScript\CurrencyAPI\Providers\OpenExchangeRates('OPEN_EXCHANGE_RATES_API')
+);
+
+
+//If we are going to use fiat and deposit will be in Litecoin (LTC):
+$button_code = $metamaskIPN->from_fiat(10, 'PEN')
+	->to_crypto('LTC')
+	->site_id(0)
+	->custom_1('my_optional_parameter')
+	->custom_2('other_option_parameter')
+	->custom_3('another_option_parameter')
+	->lang('en') //For supported languages go to https://metamaskipn.com/docs
+	->button_code('Pay Now');
+
+echo $button_code;
+
+//If we are going to use a cryptocurrency like Solana (SOL) and deposit in in BTCB (BTC)
+$button_code_2 = $metamaskIPN->from_crypto(1,'SOL')
+	->to_crypto('BTCB')
+	->site_id(0)
+	->custom_1('custom_parameter')
+	->lang('es')
+	->button_code();
+echo $button_code_2;
+
+//If we are going to use the shopping cart URL, then we can specify item name and other items.
+$shopping_car_url = $metamaskIPN->from_crypto(55,'USD')
+	->to_crypto('BNB')
+	->site_id(0)
+	->custom_1('custom_parameter')
+	->lang('en')
+	->item_name('Make a donation')
+	->item_description('Support our project')
+	->logo('https://www.evolutionscript.com/assets/evolution/images/logo.png') //URL of your logo
+	->cancel_url('https://mysite.com/cart')
+	->success_url('https://mysite.com/success')
+	->shopping_cart();
+echo '<br><a href="'.$shopping_car_url.'">Click here to redirect to shopping cart</a>';
+```
---
+++ src/MetamaskIPN.php
@@ -0,0 +1,238 @@
+<?php
+namespace EvolutionScript\MetamaskIPN;
+
+
+use EvolutionScript\CryptocurrencyAPI\Exchange;
+use EvolutionScript\CurrencyAPI\Rate;
+
+class MetamaskIPN
+{
+	private $currency_converter;
+	private $crypto_api;
+	private $base_currency = 'USD';
+	private $fiat_amount = 0;
+	private $deposit_amount = 0;
+	private $site_id = 0;
+	private $token = '';
+	private $custom_1 = null;
+	private $custom_2 = null;
+	private $custom_3 = null;
+	private $lang = 'en';
+	private $item_name = '';
+	private $item_description = '';
+	private $logo = '';
+	private $cancel_url = '';
+	private $success_url = '';
+
+	public function __construct()
+	{
+		$this->currency_converter = new Rate();
+		$this->crypto_api = new Exchange();
+	}
+
+	public function cacheDirectory($cache_dir)
+	{
+		$this->currency_converter->cacheDirectory($cache_dir);
+	}
+
+	public function currencyProviders($service_provider, $backup_provider=null)
+	{
+		$this->currency_converter->provider($service_provider, $backup_provider);
+	}
+
+	public function base_currency($base_currency)
+	{
+		$this->base_currency = strtoupper($base_currency);
+	}
+
+	public function from_fiat($amount, $currency)
+	{
+		$currency = strtoupper($currency);
+		$rate = $this->currency_converter->getRate($this->base_currency, $currency);
+		$this->fiat_amount = $this->currency_converter->exchangeTo($rate, $amount);
+		return $this;
+	}
+
+	public function from_crypto($amount, $currency)
+	{
+		if(in_array($currency, ['USDT','USD','BUSD','USDC','DAI'])){
+			$this->fiat_amount = $amount;
+		}else{
+			$rate = $this->crypto_api->rate($currency, 'USDT');
+			$this->fiat_amount = $this->crypto_api->exchangeTo($amount, $rate);
+		}
+		return $this;
+	}
+
+	public function to_crypto($token)
+	{
+		if(!array_key_exists($token, $this->getTokens())){
+			throw new \Exception('Token is not supported by MetamaskIPN.');
+		}
+		$this->token = $token;
+		$crypto_currency = $this->getTokens()[$token];
+		if($crypto_currency == 'USD'){
+			$this->deposit_amount = $this->fiat_amount;
+		}else{
+			$rate = $this->crypto_api->rate($crypto_currency, 'USDT');
+			//1 Crypto = $rate
+			$this->deposit_amount = $this->crypto_api->exchangeFrom($this->fiat_amount, $rate);
+		}
+		return $this;
+	}
+	private function getTokens()
+	{
+		return [
+			'BNB' => 'BNB',
+			'BUSD' => 'USD',
+			'BTCB' => 'BTC',
+			'ETH' => 'ETH',
+			'LTC' => 'LTC',
+			'USDT' => 'USD',
+			'Cake' => 'CAKE',
+			'UNI' => 'UNI'
+		];
+	}
+
+	public function site_id($site_id)
+	{
+		if(!isset($site_id) || !is_numeric($site_id)){
+			throw new \Exception('MetamaskIPN site ID is not valid.');
+		}
+		$this->site_id = $site_id;
+		return $this;
+	}
+
+	public function custom_1($custom_1)
+	{
+		$this->custom_1 = $custom_1;
+		return $this;
+	}
+
+	public function custom_2($custom_2)
+	{
+		$this->custom_2 = $custom_2;
+		return $this;
+	}
+
+	public function custom_3($custom_3)
+	{
+		$this->custom_3 = $custom_3;
+		return $this;
+	}
+
+	public function lang($lang)
+	{
+		$this->lang = $lang;
+		return $this;
+	}
+
+	public function item_name($item_name)
+	{
+		$this->item_name = $item_name;
+		return $this;
+	}
+
+	public function item_description($item_description)
+	{
+		$this->item_description = $item_description;
+		return $this;
+	}
+
+	public function logo($logo)
+	{
+		$this->logo = $logo;
+		return $this;
+	}
+
+	public function cancel_url($cancel_url)
+	{
+		$this->cancel_url = $cancel_url;
+		return $this;
+	}
+
+	public function success_url($success_url)
+	{
+		$this->success_url = $success_url;
+		return $this;
+	}
+
+	public function button_code($button_text='')
+	{
+		$params['site'] = $this->site_id;
+		$params['token'] = $this->token;
+		$params['lang'] = $this->lang;
+		if(!is_null($this->custom_1) || $this->custom_1 != ''){
+			$params['custom_1'] = $this->custom_1;
+		}
+		if(!is_null($this->custom_2) || $this->custom_2 != ''){
+			$params['custom_2'] = $this->custom_2;
+		}
+		if(!is_null($this->custom_3) || $this->custom_3 != ''){
+			$params['custom_3'] = $this->custom_3;
+		}
+		if($button_text != ''){
+			$params['btn_txt'] = $button_text;
+		}
+		if($this->deposit_amount <= 0){
+			throw new \Exception('Invalid deposit amount.');
+		}
+		$params['amount'] = $this->deposit_amount;
+		$query = http_build_query($params);
+		$code = '<iframe src="https://metamaskipn.com/button?'.$query.'" style="border: 0; overflow: hidden"></iframe>';
+		$this->reset_params();
+		return $code;
+	}
+
+	public function shopping_cart()
+	{
+		$params['site'] = $this->site_id;
+		$params['token'] = $this->token;
+		$params['lang'] = $this->lang;
+		if(!is_null($this->custom_1) || $this->custom_1 != ''){
+			$params['custom_1'] = $this->custom_1;
+		}
+		if(!is_null($this->custom_2) || $this->custom_2 != ''){
+			$params['custom_2'] = $this->custom_2;
+		}
+		if(!is_null($this->custom_3) || $this->custom_3 != ''){
+			$params['custom_3'] = $this->custom_3;
+		}
+		if($this->item_name == ''){
+			throw new \Exception('item_name is required.');
+		}
+		$params['item_name'] = $this->item_name;
+		if($this->item_description !== ''){
+			$params['item_description'] = $this->item_description;
+		}
+		if(filter_var($this->logo, FILTER_VALIDATE_URL)){
+			$params['logo'] = $this->logo;
+		}
+		if(filter_var($this->cancel_url, FILTER_VALIDATE_URL)){
+			$params['cancel_url'] = $this->cancel_url;
+		}
+		if(filter_var($this->success_url, FILTER_VALIDATE_URL)){
+			$params['success_url'] = $this->success_url;
+		}
+		if($this->deposit_amount <= 0){
+			throw new \Exception('Invalid deposit amount.');
+		}
+		$params['amount'] = $this->deposit_amount;
+		$query = http_build_query($params);
+		$code = 'https://metamaskipn.com/cart?'.$query;
+		$this->reset_params();
+		return $code;
+	}
+
+	private function reset_params()
+	{
+		$this->fiat_amount = 0;
+		$this->deposit_amount = 0;
+		$this->site_id = 0;
+		$this->token = '';
+		$this->custom_1 = null;
+		$this->custom_2 = null;
+		$this->custom_3 = null;
+		$this->lang = 'en';
+	}
+}
